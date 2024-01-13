0ef290b7918691ebd169e8df49e00e62d3e0ab2a
JZeedo <jameskstanton123@gmail.com>
2024-01-13 22:37:49 +0200

    Create function to read words from textfile and put it in a list

--- game.py
+++ game.py
@@ -2,3 +2,12 @@ MAX_MISTAKES = 7
 game_over = False
 mistakes = 0
 attempts = 0
+wordlist = []
+
+def read_file(fileName):
+    tfl = open(fileName)
+    words = tfl.readlines()
+    tfl.close
+    for k in range (len(words)-1):
+        words[k] = words[k][:-1]
+    return words
